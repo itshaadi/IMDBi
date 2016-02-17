1a3126fe37e6e849ea5a4c3d12075c0d0556671a
mohamad azami <iazami@outlook.com>
2016-02-17 23:59:24 +0330

    refactor imdbi function

--- public/imdbi-public-functions.php
+++ public/imdbi-public-functions.php
@@ -70,73 +70,13 @@ function imdbi($name){
 
   if($name == "poster" ){
     if( !empty( get_post_meta($post->ID, 'IMDBI_Poster', true) ) ){
-    echo get_post_meta( $post->ID, 'IMDBI_Poster', true );
-    return;
-    }
-    elseif(empty( get_post_meta($post->ID, 'IMDBI_Poster', true) )){
-      echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); // if there is no POSTER it will print thumbnail url
-      return;
-    }
-    else{
-      return _e('N/A','imdbi');
-    }
-  }
-
-  /* List of meta box fields (meta_name => meta_key) */
-  $fields = array(
-    'imdbid'		 => 'imdbID',
-    'title'	 		 => 'IMDBI_Title',
-    'year'	 		 => 'IMDBI_Year',
-    'type'	 		 => 'IMDBI_Type',
-    'trailer' 	 => 'IMDBI_Trailer',
-    'budget' 		 => 'IMDBI_Budget',
-    'gross'			 => 'IMDBI_Gross',
-    'imdbvotes'  => 'IMDBI_imdbVotes',
-    'imdbrating' => 'IMDBI_imdbRating',
-    'metascore'	 => 'IMDBI_Metascore',
-    'actors'		 => 'IMDBI_Actors',
-    'writer'		 => 'IMDBI_Writer',
-    'director'	 => 'IMDBI_Director',
-    'runtime' 	 => 'IMDBI_Runtime',
-    'released'	 => 'IMDBI_Released',
-    'rated' 		 => 'IMDBI_Rated',
-    'plot' 			 => 'IMDBI_Plot',
-    'awards'		 => 'IMDBI_Awards',
-    'language'	 => 'IMDBI_Language',
-    'country' 	 => 'IMDBI_Counetry',
-    'genre'			 => 'IMDBI_Genre'
-  );
-
-  /* Indolence is the key :) who need switch cases ? */
-  foreach($fields as $meta_name => $meta_key){
-    if( $name == $meta_name && !empty( get_post_meta( $post->ID, $meta_key, true ) ) ){
-      echo get_post_meta( $post->ID, $meta_key, true );
-      return;
-    }
-    else{
-      continue;
-    }
-  }
-  return _e('N/A','imdbi'); // so instead of NULL you can return a proper message in any language.
-}
-
-
-
-
-/* this function will return informations that saved in meta box */
-function imdbi_fetch_meta($name){
-  global $post;
-  $name = trim(strtolower($name));
-
-  if($name == "poster" ){
-    if( !empty( get_post_meta($post->ID, 'IMDBI_Poster', true) ) ){
     return get_post_meta( $post->ID, 'IMDBI_Poster', true );
     }
     elseif(empty( get_post_meta($post->ID, 'IMDBI_Poster', true) )){
       return wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); // if there is no POSTER it will print thumbnail url
     }
     else{
-      return _e('N/A','imdbi');
+      return __('N/A','imdbi');
     }
   }
 
@@ -174,7 +114,64 @@ function imdbi_fetch_meta($name){
       continue;
     }
   }
-  return _e('N/A','imdbi'); // so instead of NULL you can return a proper message in any language.
+  return __('N/A','imdbi'); // so instead of NULL you can return a proper message in any language.
+}
+
+
+
+
+/* this function will return informations that saved in meta box */
+function imdbi_fetch_meta($name){
+  global $post;
+  $name = trim(strtolower($name));
+
+  if($name == "poster" ){
+    if( !empty( get_post_meta($post->ID, 'IMDBI_Poster', true) ) ){
+    return get_post_meta( $post->ID, 'IMDBI_Poster', true );
+    }
+    elseif(empty( get_post_meta($post->ID, 'IMDBI_Poster', true) )){
+      return wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); // if there is no POSTER it will print thumbnail url
+    }
+    else{
+      return __('N/A','imdbi');
+    }
+  }
+
+  /* List of meta box fields (meta_name => meta_key) */
+  $fields = array(
+    'imdbid'		 => 'imdbID',
+    'title'	 		 => 'IMDBI_Title',
+    'year'	 		 => 'IMDBI_Year',
+    'type'	 		 => 'IMDBI_Type',
+    'trailer' 	 => 'IMDBI_Trailer',
+    'budget' 		 => 'IMDBI_Budget',
+    'gross'			 => 'IMDBI_Gross',
+    'imdbvotes'  => 'IMDBI_imdbVotes',
+    'imdbrating' => 'IMDBI_imdbRating',
+    'metascore'	 => 'IMDBI_Metascore',
+    'actors'		 => 'IMDBI_Actors',
+    'writer'		 => 'IMDBI_Writer',
+    'director'	 => 'IMDBI_Director',
+    'runtime' 	 => 'IMDBI_Runtime',
+    'released'	 => 'IMDBI_Released',
+    'rated' 		 => 'IMDBI_Rated',
+    'plot' 			 => 'IMDBI_Plot',
+    'awards'		 => 'IMDBI_Awards',
+    'language'	 => 'IMDBI_Language',
+    'country' 	 => 'IMDBI_Counetry',
+    'genre'			 => 'IMDBI_Genre'
+  );
+
+  /* Indolence is the key :) who need switch cases ? */
+  foreach($fields as $meta_name => $meta_key){
+    if( $name == $meta_name && !empty( get_post_meta( $post->ID, $meta_key, true ) ) ){
+      return get_post_meta( $post->ID, $meta_key, true );
+    }
+    else{
+      continue;
+    }
+  }
+  return __('N/A','imdbi'); // so instead of NULL you can return a proper message in any language.
 }
 
 
